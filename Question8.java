6dc66ec37707cfc6096592b6ee967a1c606d9052
Omer575 <71461646+Omer575@users.noreply.github.com>
2020-09-19 21:08:34 +0200

    Update Question8.java

--- Question8.java
+++ Question8.java
@@ -4,8 +4,8 @@ public class Question8 {
 
 	public static void main(String[] args) {
 		
-		String progLanguage1 = "Java";
-		String progLanguage2 = "SQL";
+		String progLanguage1 = "\"Java\"";
+		String progLanguage2 = "\"SQL\"";
 		
 		System.out.println("I am learning " + progLanguage1 + " & " + progLanguage2 + " from the best school named Cybertekschool !");
 
